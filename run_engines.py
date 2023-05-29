a79fb21bbe24ec68434257dd33e3cff496203045
Carlos <cibarcena4@outlook.com>
2023-05-29 08:52:53 +0800

    fix error running engines

--- run_engines.py
+++ run_engines.py
@@ -1,18 +1,22 @@
 import yaml
 import subprocess
 
-# Update basic configuration
-with open('config.yaml', 'r') as file:
-    config = yaml.safe_load(file)
+try:
+    # Update basic configuration
+    with open('config.yaml', 'r') as file:
+        config = yaml.safe_load(file)
 
-# Run commands
-commands = [["streamlit", "run", "Home.py", "--server.port=8501", "--server.maxUploadSize=25"], ['nohup', 'python', 'run_telegram.py']]
+    # Run commands
+    commands = [["streamlit", "run", "Home.py", "--server.port=8501", "--server.maxUploadSize=25"], ['nohup', 'python', 'run_telegram.py']]
 
-for i in range(int(config['engines']['large'])):
-    commands.append(['python', 'run_extra.py', 'large'])
+    for i in range(int(config['engines']['large'])):
+        commands.append(['python', 'run_extra.py', 'large'])
 
-for i in range(int(config['engines']['short'])):
-    commands.append(['python', 'run_extra.py', 'short'])
-    
-for command in commands:
-    subprocess.Popen(command)
+    for i in range(int(config['engines']['short'])):
+        commands.append(['python', 'run_extra.py', 'short'])
+        
+    for command in commands:
+        subprocess.Popen(command)
+        print("Running engine")
+except:
+    print("Engines not running, please complete initial setup")
