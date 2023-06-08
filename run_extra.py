26cf306916f48443fa73358e1e1b670984141444
Carlos <cibarcena4@outlook.com>
2023-06-08 14:33:35 +0800

    update

--- run_extra.py
+++ run_extra.py
@@ -30,85 +30,89 @@ def put_extra(job):
 def run_short(BOT_TOKEN, extra, admin_url):
     for m in range(len(extra['short'])):
         extra_data = extra['short'][m].split('|')
-
-        with open('idiom.yaml', 'r') as file:
-            idio = yaml.safe_load(file)
-        user_data = open_data(extra_data[0])
-        idi = user_data['idiom']
-        if idi not in list(idio['Add homework'].keys()):
-            idi = 'en'
-
-        if 'img2text' in extra_data: # user_id|img2text
-            image_path = 'miniapps/languages/images/'+extra_data[0]
-            image = Image.open(image_path)
-            text = pytesseract.image_to_string(image)
-            os.remove(image_path)
-            put_extra(f"{extra_data[0]}|message|{text}")
-            take_extra('short', m)
-
-        elif 'youtube' in extra_data:
-            cb_data = extra_data[4].split("*") # cb_data => user_id*resolution*bitrate*fps
-            size_exceeded = f"{idio['File size exceeded, download the file here'][idi]}:\n{admin_url}"
-            del_list = ['miniapps/youtube/'+cb_data[0]+'a', 'miniapps/youtube/'+cb_data[0]+'v']
-
-            if len(cb_data) == 4: # there is fps information
-                yt = YouTube(user_data['miniapps']['youtube']['request']['url']).streams
-                yt.filter(type="video", res=cb_data[1], fps=int(cb_data[3]))[0].download(output_path='miniapps/youtube/', filename=cb_data[0]+'v')
-                yt.filter(type="audio", abr=cb_data[2])[0].download(output_path='miniapps/youtube/', filename=cb_data[0]+'a')
-                subprocess.run(['ffmpeg', '-loglevel', 'quiet', '-i', 'miniapps/youtube/'+cb_data[0]+'v', '-i', 'miniapps/youtube/'+cb_data[0]+'a', '-c:v', 'copy', '-c:a', 'libmp3lame', '-b:a',  cb_data[2].split('b')[0], '-strict', '-2', 'miniapps/youtube/'+clean_text(extra_data[3])+'.mp4', '-y'])
-                del_list.append('miniapps/youtube/'+cb_data[0]+'.mp3')
-                put_extra(f"{extra_data[0]}|video|{extra_data[3]}|miniapps/youtube/{clean_text(extra_data[3])}.mp4|{size_exceeded}")
-
-            elif 'audio' in cb_data: # is only audio
-                yt = YouTube(user_data['miniapps']['youtube']['request']['url']).streams
-                yt.filter(type="audio", abr=cb_data[2])[0].download(output_path='miniapps/youtube/', filename=cb_data[0]+'a')
-                subprocess.run(['ffmpeg', '-loglevel', 'quiet', '-i', 'miniapps/youtube/'+cb_data[0]+'a', '-c:a', 'libmp3lame', '-b:a',  cb_data[2].split('b')[0], '-strict', '-2', 'miniapps/youtube/'+clean_text(extra_data[3])+'.mp3', '-y'])
-                del_list.append('miniapps/youtube/'+cb_data[0]+'.mp4')
-                put_extra(f"{extra_data[0]}|audio|{extra_data[3]}|miniapps/youtube/{clean_text(extra_data[3])}.mp3|{size_exceeded}")
-
-            else: # regular video
-                yt = YouTube(user_data['miniapps']['youtube']['request']['url']).streams
-                yt.filter(type="video", res=cb_data[1])[0].download(output_path='miniapps/youtube/', filename=cb_data[0]+'v')
-                yt.filter(type="audio", abr=cb_data[2])[0].download(output_path='miniapps/youtube/', filename=cb_data[0]+'a')
-                subprocess.run(['ffmpeg', '-loglevel', 'quiet', '-i', 'miniapps/youtube/'+cb_data[0]+'v', '-i', 'miniapps/youtube/'+cb_data[0]+'a', '-c:v', 'copy', '-c:a', 'libmp3lame', '-b:a',  cb_data[2].split('b')[0], '-strict', '-2', 'miniapps/youtube/'+clean_text(extra_data[3])+'.mp4', '-y'])
-                del_list.append('miniapps/youtube/'+cb_data[0]+'.mp3')
-                put_extra(f"{extra_data[0]}|video|{extra_data[3]}|miniapps/youtube/{clean_text(extra_data[3])}.mp4|{size_exceeded}")
-
-            for del_file in del_list:
-                try:
-                    os.remove(del_file)
-                except:
-                    pass
-            take_extra('short', m)
         
-        elif 'read_speak' in extra_data: # extra_data = user_id|read_speak|voice_file_id|reply
-            user_data, message = read_speak(BOT_TOKEN, extra_data[0], extra_data[2], idio, idi)
-            put_extra(f"{extra_data[0]}|message_reply|{message}|{extra_data[3]}")
-            take_extra('short', m)
+        if extra_data != []:
+
+            with open('idiom.yaml', 'r') as file:
+                idio = yaml.safe_load(file)
+            user_data = open_data(extra_data[0])
+            idi = user_data['idiom']
+            if idi not in list(idio['Add homework'].keys()):
+                idi = 'en'
+
+            if 'img2text' in extra_data: # user_id|img2text
+                image_path = 'miniapps/languages/images/'+extra_data[0]
+                image = Image.open(image_path)
+                text = pytesseract.image_to_string(image)
+                os.remove(image_path)
+                put_extra(f"{extra_data[0]}|message|{text}")
+                take_extra('short', m)
+
+            elif 'youtube' in extra_data:
+                cb_data = extra_data[4].split("*") # cb_data => user_id*resolution*bitrate*fps
+                size_exceeded = f"{idio['File size exceeded, download the file here'][idi]}:\n{admin_url}"
+                del_list = ['miniapps/youtube/'+cb_data[0]+'a', 'miniapps/youtube/'+cb_data[0]+'v']
+
+                if len(cb_data) == 4: # there is fps information
+                    yt = YouTube(user_data['miniapps']['youtube']['request']['url']).streams
+                    yt.filter(type="video", res=cb_data[1], fps=int(cb_data[3]))[0].download(output_path='miniapps/youtube/', filename=cb_data[0]+'v')
+                    yt.filter(type="audio", abr=cb_data[2])[0].download(output_path='miniapps/youtube/', filename=cb_data[0]+'a')
+                    subprocess.run(['ffmpeg', '-loglevel', 'quiet', '-i', 'miniapps/youtube/'+cb_data[0]+'v', '-i', 'miniapps/youtube/'+cb_data[0]+'a', '-c:v', 'copy', '-c:a', 'libmp3lame', '-b:a',  cb_data[2].split('b')[0], '-strict', '-2', 'miniapps/youtube/'+clean_text(extra_data[3])+'.mp4', '-y'])
+                    del_list.append('miniapps/youtube/'+cb_data[0]+'.mp3')
+                    put_extra(f"{extra_data[0]}|video|{extra_data[3]}|miniapps/youtube/{clean_text(extra_data[3])}.mp4|{size_exceeded}")
+
+                elif 'audio' in cb_data: # is only audio
+                    yt = YouTube(user_data['miniapps']['youtube']['request']['url']).streams
+                    yt.filter(type="audio", abr=cb_data[2])[0].download(output_path='miniapps/youtube/', filename=cb_data[0]+'a')
+                    subprocess.run(['ffmpeg', '-loglevel', 'quiet', '-i', 'miniapps/youtube/'+cb_data[0]+'a', '-c:a', 'libmp3lame', '-b:a',  cb_data[2].split('b')[0], '-strict', '-2', 'miniapps/youtube/'+clean_text(extra_data[3])+'.mp3', '-y'])
+                    del_list.append('miniapps/youtube/'+cb_data[0]+'.mp4')
+                    put_extra(f"{extra_data[0]}|audio|{extra_data[3]}|miniapps/youtube/{clean_text(extra_data[3])}.mp3|{size_exceeded}")
+
+                else: # regular video
+                    yt = YouTube(user_data['miniapps']['youtube']['request']['url']).streams
+                    yt.filter(type="video", res=cb_data[1])[0].download(output_path='miniapps/youtube/', filename=cb_data[0]+'v')
+                    yt.filter(type="audio", abr=cb_data[2])[0].download(output_path='miniapps/youtube/', filename=cb_data[0]+'a')
+                    subprocess.run(['ffmpeg', '-loglevel', 'quiet', '-i', 'miniapps/youtube/'+cb_data[0]+'v', '-i', 'miniapps/youtube/'+cb_data[0]+'a', '-c:v', 'copy', '-c:a', 'libmp3lame', '-b:a',  cb_data[2].split('b')[0], '-strict', '-2', 'miniapps/youtube/'+clean_text(extra_data[3])+'.mp4', '-y'])
+                    del_list.append('miniapps/youtube/'+cb_data[0]+'.mp3')
+                    put_extra(f"{extra_data[0]}|video|{extra_data[3]}|miniapps/youtube/{clean_text(extra_data[3])}.mp4|{size_exceeded}")
+
+                for del_file in del_list:
+                    try:
+                        os.remove(del_file)
+                    except:
+                        pass
+                take_extra('short', m)
+            
+            elif 'read_speak' in extra_data: # extra_data = user_id|read_speak|voice_file_id|reply
+                user_data, message = read_speak(BOT_TOKEN, extra_data[0], extra_data[2], idio, idi)
+                put_extra(f"{extra_data[0]}|message_reply|{message}|{extra_data[3]}")
+                take_extra('short', m)
 
 
 def run_large(BOT_TOKEN, extra, admin_url):
     for m in range(len(extra['large'])):
         extra_data = extra['large'][m].split('|') # user_id|voice2text|whisper|tiny
 
-        with open('idiom.yaml', 'r') as file:
-            idio = yaml.safe_load(file)
-        user_data = open_data(extra_data[0])
-        idi = user_data['idiom']
-        if idi not in list(idio['Add homework'].keys()):
-            idi = 'en'
+        if extra_data != []:
 
-        if 'voice2text' in extra_data: # user_id|voice2text|whisper|tiny
-            media_file_path = f"miniapps/voice2text/{extra_data[0]}"
-            text = generate_transcription(extra_data[3], media_file_path)
-            os.remove(media_file_path)
-            put_extra(f"{extra_data[0]}|message|{text}")
-            take_extra('large', m)
+            with open('idiom.yaml', 'r') as file:
+                idio = yaml.safe_load(file)
+            user_data = open_data(extra_data[0])
+            idi = user_data['idiom']
+            if idi not in list(idio['Add homework'].keys()):
+                idi = 'en'
+
+            if 'voice2text' in extra_data: # user_id|voice2text|whisper|tiny
+                media_file_path = f"miniapps/voice2text/{extra_data[0]}"
+                text = generate_transcription(extra_data[3], media_file_path)
+                os.remove(media_file_path)
+                put_extra(f"{extra_data[0]}|message|{text}")
+                take_extra('large', m)
 
 
 def main(BOT_TOKEN, arg1, admin_url):
     while True:
-        time.sleep(0.5) # to not overload the processor
+        time.sleep(1) # to not overload the processor
         with open('extra.yaml', 'r') as file:
             extra = yaml.safe_load(file)
 
